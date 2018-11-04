3ba3a375ceba6b0b6b148ded802714b60611107c
Sean Lucius <seanlucius22@icloud.com>
2018-11-04 19:02:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
-counter = 0
+
 def turn_count(board)
+  counter = 0 
   board.each do |space|
     if space == "X" || space == "O"
-      counter++
+      counter += 1 
   end 
   return counter
 end 
